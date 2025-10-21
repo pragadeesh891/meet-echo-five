40d836573e11a9880581fba9773e555c69ec6bc1
Tharun kamalesh <tharunkamalesh09@gmail.com>
2025-10-21 11:28:41 +0530

    Add root configuration files

---
+++ interface_config.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-unused-vars, no-var, max-len */
+/* eslint sort-keys: ["error", "asc", {"caseSensitive": false}] */
+
+/**
+ * Interface configuration for Echo Five Meet
+ */
+
+var interfaceConfig = {
+    APP_NAME: 'ECHO-FIVE-MEET',
+    AUDIO_LEVEL_PRIMARY_COLOR: 'rgba(255,255,255,0.4)',
+    AUDIO_LEVEL_SECONDARY_COLOR: 'rgba(255,255,255,0.2)',
+    AUTO_PIN_LATEST_SCREEN_SHARE: 'remote-only',
+
+    BRAND_WATERMARK_LINK: '',
+    CLOSE_PAGE_GUEST_HINT: false,
+    DEFAULT_BACKGROUND: '#040404',
+    DEFAULT_WELCOME_PAGE_LOGO_URL: 'images/watermark.svg',
+
+    DISABLE_DOMINANT_SPEAKER_INDICATOR: false,
+    DISABLE_JOIN_LEAVE_NOTIFICATIONS: false,
+    DISABLE_PRESENCE_STATUS: false,
+    DISABLE_TRANSCRIPTION_SUBTITLES: false,
+    DISABLE_VIDEO_BACKGROUND: false,
+
+    DISPLAY_WELCOME_FOOTER: true,
+    DISPLAY_WELCOME_PAGE_ADDITIONAL_CARD: false,
+    DISPLAY_WELCOME_PAGE_CONTENT: false,
+    DISPLAY_WELCOME_PAGE_TOOLBAR_ADDITIONAL_CONTENT: false,
+
+    ENABLE_DIAL_OUT: true,
+    FILM_STRIP_MAX_HEIGHT: 120,
+    GENERATE_ROOMNAMES_ON_WELCOME_PAGE: true,
+    HIDE_INVITE_MORE_HEADER: false,
+
+    JITSI_WATERMARK_LINK: '',
+    LANG_DETECTION: true,
+    LOCAL_THUMBNAIL_RATIO: 16 / 9,
+    MAXIMUM_ZOOMING_COEFFICIENT: 1.3,
+    MOBILE_APP_PROMO: true,
+
+    OPTIMAL_BROWSERS: ['chrome', 'chromium', 'firefox', 'electron', 'safari', 'webkit'],
+
+    POLICY_LOGO: null,
+    PROVIDER_NAME: 'ECHO-FIVE-MEET',
+
+    RECENT_LIST_ENABLED: true,
+    REMOTE_THUMBNAIL_RATIO: 1,
+    SETTINGS_SECTIONS: ['devices', 'language', 'moderator', 'profile', 'calendar', 'sounds', 'more'],
+
+    SHOW_BRAND_WATERMARK: false,
+    SHOW_CHROME_EXTENSION_BANNER: false,
+    SHOW_JITSI_WATERMARK: false,
+    SHOW_POWERED_BY: false,
+    SHOW_PROMOTIONAL_CLOSE_PAGE: false,
+
+    SUPPORT_URL: '',
+    UNSUPPORTED_BROWSERS: [],
+    VERTICAL_FILMSTRIP: true,
+    VIDEO_LAYOUT_FIT: 'both',
+    VIDEO_QUALITY_LABEL_DISABLED: false,
+
+    makeJsonParserHappy: 'even if last key had a trailing comma'
+};
+
+/* eslint-enable no-unused-vars, no-var, max-len */
